2e6aab335fce247a356ec5847a1f4cfd4aed38f3
maritzacg1 <kchalen14@gmail.com>
2025-11-13 23:09:16 -0500

    actualizar api

--- src/jwt/verifytoken.js
+++ src/jwt/verifytoken.js
@@ -4,7 +4,7 @@ import { JWT_SECRET } from '../config.js';
 // Token estático por defecto (tu token actual)eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
 
 
-const DEFAULT_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzYzMDAwMzY4LCJleHAiOjE3NjMwODY3Njh9.HPmMCqLUB6w3PFTOlAJr2VOfthgzORGTv8bHIsoOpZw";
+const DEFAULT_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzYzMDkzMzI2LCJleHAiOjE3NjMxNzk3MjZ9.36J9Rt5mi4_sRyfSk1BvRKUVwkj818ov7tPIQZZHCXQ";
 
 export const verifyToken = async (req, res, next) => {
     let token;
